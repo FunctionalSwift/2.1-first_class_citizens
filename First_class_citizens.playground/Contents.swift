4ffeeb2c92ad8aa4d02b6693bd7cf859fbc7059b
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-04 01:48:07 +0100

    We need to start it again, let's go to first step!

--- First_class_citizens.playground/Contents.swift
+++ First_class_citizens.playground/Contents.swift
@@ -2,39 +2,27 @@
 
 import Foundation
 
-protocol Parser {
-    func parse(_ response: String) -> [String: AnyObject]
-}
-
-class JSONParser: Parser {
-    func parse(_ response: String) -> [String : AnyObject] {
-        let data = response.data(using: .utf8)!
-        
-        return try! JSONSerialization.jsonObject(with: data) as! [String: AnyObject]
-    }
-}
-
-class XMLParser: Parser {
-    func parse(_ response: String) -> [String : AnyObject] {
-        return [:]
-    }
-}
-
-class HTMLParser: Parser {
-    func parse(_ response: String) -> [String : AnyObject] {
-        return [:]
-    }
-}
-
-func data(from url: URL, parser: Parser) -> [String: AnyObject] {
+func parseAsJSON(_ response: String) -> [String: AnyObject] {
+    let data = response.data(using: .utf8)!
     
+    return try! JSONSerialization.jsonObject(with: data) as! [String: AnyObject]
+}
+
+func parseAsXML(_ response: String) -> [String: AnyObject] {
+    return [:]
+}
+
+func parseAsHTML(_ response: String) -> [String: AnyObject] {
+    return [:]
+}
+
+func data(from url: URL) -> [String: AnyObject] {
     let data = NSData(contentsOf: url)! as Data
     let content = String(data: data, encoding: .utf8)!
     
-    return parser.parse(content)
+    return parseAsJSON(content)
 }
 
 let url = URL(string: "http://swiftfuncional.com/exercises/posts/1")!
 
-data(from: url, parser: JSONParser())
-
+data(from: url)
