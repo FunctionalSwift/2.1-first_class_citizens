1c66340c37196c38bf03d17ac94a5ff02dead1fe
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-04 01:35:10 +0100

    What about other types of data?

--- First_class_citizens.playground/Contents.swift
+++ First_class_citizens.playground/Contents.swift
@@ -2,10 +2,19 @@
 
 import Foundation
 
-func parse(_ response: String) -> [String: AnyObject] {
+func parseAsJSON(_ response: String) -> [String: AnyObject] {
     let data = response.data(using: .utf8)!
-
+    
     return try! JSONSerialization.jsonObject(with: data) as! [String: AnyObject]
 }
 
-parse("{\"data\": 42}")
+func parseAsXML(_ response: String) -> [String: AnyObject] {
+    return [:]
+}
+
+func parseAsHTML(_ response: String) -> [String: AnyObject] {
+    return [:]
+}
+
+
+parseAsJSON("{\"data\": 42}")
