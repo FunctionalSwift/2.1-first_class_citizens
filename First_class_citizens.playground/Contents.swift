69df76d442545466397fee72cfdf18518219c396
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-04 01:33:53 +0100

    Just thinking about JSON

--- First_class_citizens.playground/Contents.swift
+++ First_class_citizens.playground/Contents.swift
@@ -1,1 +1,11 @@
 //: Playground - First_class_citizens
+
+import Foundation
+
+func parse(_ response: String) -> [String: AnyObject] {
+    let data = response.data(using: .utf8)!
+
+    return try! JSONSerialization.jsonObject(with: data) as! [String: AnyObject]
+}
+
+parse("{\"data\": 42}")
