c900023049a258082dffa3d957c00211617d8e6e
Roberto Gómez Muñoz <rob.gomez.mu@gmail.com>
2018-03-04 01:44:53 +0100

    Mmm. Wait! This is starting to smell like imperative programming... Are you sure this is the best solution?

--- First_class_citizens.playground/Contents.swift
+++ First_class_citizens.playground/Contents.swift
@@ -2,38 +2,39 @@
 
 import Foundation
 
-func parseAsJSON(_ response: String) -> [String: AnyObject] {
-    let data = response.data(using: .utf8)!
-    
-    return try! JSONSerialization.jsonObject(with: data) as! [String: AnyObject]
+protocol Parser {
+    func parse(_ response: String) -> [String: AnyObject]
 }
 
-func parseAsXML(_ response: String) -> [String: AnyObject] {
-    return [:]
+class JSONParser: Parser {
+    func parse(_ response: String) -> [String : AnyObject] {
+        let data = response.data(using: .utf8)!
+        
+        return try! JSONSerialization.jsonObject(with: data) as! [String: AnyObject]
+    }
 }
 
-func parseAsHTML(_ response: String) -> [String: AnyObject] {
-    return [:]
-}
-
-func data(from url: URL, type: Int) -> [String: AnyObject] {
-    let data = NSData(contentsOf: url)! as Data
-    let content = String(data: data, encoding: .utf8)!
-    
-    switch type {
-    case 1:
-        return parseAsJSON(content)
-    case 2:
-        return parseAsXML(content)
-    case 3:
-        return parseAsHTML(content)
-    default:
+class XMLParser: Parser {
+    func parse(_ response: String) -> [String : AnyObject] {
         return [:]
     }
 }
 
+class HTMLParser: Parser {
+    func parse(_ response: String) -> [String : AnyObject] {
+        return [:]
+    }
+}
+
+func data(from url: URL, parser: Parser) -> [String: AnyObject] {
+    
+    let data = NSData(contentsOf: url)! as Data
+    let content = String(data: data, encoding: .utf8)!
+    
+    return parser.parse(content)
+}
+
 let url = URL(string: "http://swiftfuncional.com/exercises/posts/1")!
 
-data(from: url, type: 1)
-
+data(from: url, parser: JSONParser())
 
